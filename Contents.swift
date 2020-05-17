702502a3e42cfcd7d8cfc59b6e1dee731eceed66
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-05-17 22:24:08 +0530

    Add heading

--- Contents.swift
+++ Contents.swift
@@ -4,15 +4,63 @@ import PlaygroundSupport
 //let vc = LaunchViewController()
 //vc.preferredContentSize = CGSize(width:600, height:400)
 //PlaygroundPage.current.liveView = vc
-let vc = IntroViewController()
+let vc = IntroViewController1()
 vc.preferredContentSize = CGSize(width:600, height:400)
 PlaygroundPage.current.liveView = vc
 
-public class IntroViewController : UIViewController{
-    let logo = UIImageView()
+public class IntroViewController1 : UIViewController{
+    //    let newViewController = IntroViewController()
+    
+    let nextButton = UIButton()
+    let headingLabel = UILabel()
+    let
+    
     public override func loadView() {
         let view = UIView()
         view.backgroundColor = .black
         self.view = view
+        setUpNextButton()
+    }
+    
+    func setUpNextButton(){
+        
+        view.addSubview(nextButton)
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.setTitleColor(.black, for: .normal)
+        nextButton.backgroundColor = .lightText
+        nextButton.layer.cornerRadius = 5
+        nextButton.layer.borderWidth = 1
+        nextButton.layer.borderColor = UIColor.white.cgColor
+        setButtonConstraints()
+        setUpTitle()
+        //        nextButton.addTarget(self,action: #selector(getStartedClicked),for: .touchUpInside)
+        //
+        //    }
+        //    @objc func getStartedClicked(){
+        //        newViewController.preferredContentSize = CGSize(width:600, height:400)
+        //        PlaygroundPage.current.liveView = newViewController
+        
+    }
+    func setUpTitle(){
+        view.addSubview(heading)
+        headingLabel.text = "What is CyberSense?"
+        headingLabel.textColor = .white
+        headingLabel.textAlignment = .center
+        headingLabel.font = UIFont(name: headingLabel.font.fontName, size: 30)
+        setTitleConstrains()
+    }
+    func setTitleConstrains(){
+        headingLabel.translatesAutoresizingMaskIntoConstraints = false
+        headingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        headingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        headingLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+    }
+    func setButtonConstraints(){
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        nextButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        nextButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30).isActive = true
     }
 }
--- Sources/IntroViewController.swift
+++ Sources/IntroViewController.swift
@@ -2,32 +2,38 @@ import UIKit
 import PlaygroundSupport
 
 public class IntroViewController : UIViewController{
-    let logo = UIImageView()
+    let newViewController = IntroViewController()
+    let nextButton = UIButton()
     public override func loadView() {
         let view = UIView()
-        view.backgroundColor = .red
+        view.backgroundColor = .black
         self.view = view
-        setLogo()
-
+        setUpNextButton()
     }
-    func setLogo(){
-          view.addSubview(logo)
-          logo.image = UIImage(named: "cybersense.png")
-          setLogoConstaints()
-          logoAnimate()
-      }
-    func setLogoConstaints(){
-           logo.translatesAutoresizingMaskIntoConstraints = false
-           logo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-           logo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-           logo.topAnchor.constraint(equalTo: view.topAnchor, constant:180).isActive = true
-           logo.heightAnchor.constraint(equalTo: logo.widthAnchor, multiplier: 1.0/5.0).isActive = true
-       }
-       
-       func logoAnimate(){
-           logo.topAnchor.constraint(equalTo: view.topAnchor, constant:80).isActive = true
-           UIView.animate(withDuration: 0.5) {
-               self.view.layoutIfNeeded()
-           }
-}
+    
+    func setUpNextButton(){
+        
+        view.addSubview(nextButton)
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.setTitleColor(.black, for: .normal)
+        nextButton.backgroundColor = .lightText
+        nextButton.layer.cornerRadius = 5
+        nextButton.layer.borderWidth = 1
+        nextButton.layer.borderColor = UIColor.white.cgColor
+        setButtonConstraints()
+        nextButton.addTarget(self,action: #selector(getStartedClicked),for: .touchUpInside)
+        
+    }
+    @objc func getStartedClicked(){
+        newViewController.preferredContentSize = CGSize(width:600, height:400)
+        PlaygroundPage.current.liveView = newViewController
+        
+    }
+    func setButtonConstraints(){
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        nextButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        nextButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30).isActive = true
+    }
 }
